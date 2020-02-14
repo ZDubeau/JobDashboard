d5c79579e10fad252239c4d5e7ecc01bc51a122b
AliceB <alice.bonnin.simplon@gmail.com>
2020-02-14 15:44:42 +0100

    Test création base et tables

---
+++ tests/test_create_table.py
@@ -0,0 +1,43 @@
+import sys
+import pytest
+
+# Manip pour permettre l'import d'un module se trouvant dans le répertoire parent
+sys.path.append("..")
+
+import create_table as ct
+
+import psycopg2
+
+def test_connexion():
+    assert ct.connect() is not None
+    assert type(ct.connect()) is psycopg2.extensions.connection
+
+
+def test_tables():
+    with ct.connect() as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) FROM offre")
+        assert cur.fetchall() == [(0,),]
+        cur.execute("SELECT COUNT(*) FROM intitule")
+        assert cur.fetchall() == [(0,),]
+        cur.execute("SELECT COUNT(*) FROM ville")
+        assert cur.fetchall() == [(0,),]
+        cur.execute("SELECT COUNT(*) FROM departement")
+        assert cur.fetchall() == [(0,),]
+        cur.execute("SELECT COUNT(*) FROM region")
+        assert cur.fetchall() == [(0,),]
+        cur.execute("SELECT COUNT(*) FROM secteur")
+        assert cur.fetchall() == [(0,),]
+        cur.execute("SELECT COUNT(*) FROM offre_secteur")
+        assert cur.fetchall() == [(0,),]
+
+
+
+
+
+
+
+
+
+
+
