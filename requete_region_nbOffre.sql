5aab9e52b72cca38842d0279376cd746880d7e6a
Zahra Dubeau <zdub@dubz.com>
2020-03-01 20:38:54 +0100

    la requete regions/nb_offre

--- requete_region_nbOffre.sql
+++ requete_region_nbOffre.sql
@@ -1,12 +1,9 @@
 SELECT region.nom as nom_region,
-ville.nom as nom_ville,
-count(*) as nb_offre,
-offre.Date_publication as date_pub,
-ville.latitude as lat,
-ville.longitude as lng
-FROM offre 
+region.code_reg as reg_code,
+count(*) as nb_offre
+FROM offre
 INNER JOIN ville ON ville.code_insee = offre.ville 
 INNER JOIN region ON region.code_reg = offre.region
-GROUP BY nom_region, nom_ville, date_pub, lat, lng
-HAVING offre.Date_publication > current_timestamp - interval '7 day'
-ORDER BY nom_region;
+WHERE offre.Date_publication > current_timestamp - interval '7 day'
+GROUP BY nom_region, reg_code
+ORDER BY nb_offre DESC;
